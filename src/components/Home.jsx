c7f61fd0b464a2b36c08b2dc6f77eb4dc8af9135
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-12-25 00:53:45 +0530

    added local storage thing in userCount

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,16 +8,21 @@ const Home = () => {
   const [userCount, setUserCount] = useState(0);
   useEffect(() => {
     const hasVisitedBefore = localStorage.getItem("hasVisitedBefore");
+    // const userCountStorage = localStorage.getItem("userCountStorage");
     if (!hasVisitedBefore) {
       localStorage.setItem("hasVisitedBefore", true);
+
       axios
         .get("https://gstsuvidhakendraserver.onrender.com/api/userCount")
         .then((res) => {
           setUserCount(res.data.userCount);
+          localStorage.setItem("userCountStorage", res.data.userCount);
         })
         .catch((error) => {
           console.log(error);
         });
+    } else {
+      setUserCount(localStorage.getItem("userCountStorage"));
     }
   }, []);
   return (
